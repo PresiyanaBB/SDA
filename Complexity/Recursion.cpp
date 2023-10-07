f10d823071896dd0e2a0be9186c47cab138255ba
Presiyana Borisova <84325049+PresiyanaBB@users.noreply.github.com>
2023-10-07 17:51:40 +0300

    Update Recursion.cpp

--- Complexity/Recursion.cpp
+++ Complexity/Recursion.cpp
@@ -52,29 +52,3 @@ int main()
 
     //fibonacci => 1 1 2 3 5 8 13 => for every n, fibonacci() will be called 2 times (for n-1 and n-2) => 2*2*2..*2 (n times) => O(2^N)
 }
-
-
-/*
-## powerSet complexity tree: ##
-f(-1)
- 1. f(0)
-     1. f(1)
-         1. f(2)
-             1. **f(3)**
-         2. f(2) incremented original
-             1. **f(3)**
-     2. f(1) incremented original
-         1. f(2)
-             1. **f(3)**
-         2. f(2)
-             1. **f(3)**
-     3. f(2) incremented original
-         1. **f(3)**
- 2. f(1) incremented original
-     1. f(2)
-         1. **f(3)**
-     2. f(2)
-         1. **f(3)**
- 3. f(2) incremented original
-     1. **f(3)**
-*/
